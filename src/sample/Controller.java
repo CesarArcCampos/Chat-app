bbf7ff6f122e277f1fbb44a01e3481d8359d8489
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-07-12 16:24:59 +0100

    Add enter event to send messages

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,6 +12,8 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -56,31 +58,19 @@ public class Controller implements Initializable {
 
         server.receiveMessageFromClient(vbox_messages);
 
+        tf_message.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode().equals(KeyCode.ENTER)) {
+                    setMessageEvent();
+                }
+            }
+        });
+
         button_send.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                String messageToSend = tf_message.getText();
-                if (!messageToSend.isEmpty()) {
-                    HBox hbox = new HBox();
-                    hbox.setAlignment(Pos.CENTER_RIGHT);
-                    hbox.setPadding(new Insets(5,5,5,10));
-
-                    Text text = new Text(messageToSend);
-                    TextFlow textFlow = new TextFlow(text);
-
-                    textFlow.setStyle("-fx-color: rgb(239,242,255); " +
-                            "-fx-background-color: rgb(15,125,242);" +
-                            " -fx-background-radius: 20px;");
-
-                    textFlow.setPadding(new Insets(5,10,5,10));
-                    text.setFill(Color.color(0.934,0.945,0.996));
-
-                    hbox.getChildren().add(textFlow);
-                    vbox_messages.getChildren().add(hbox);
-
-                    server.sendMessageToClient(messageToSend);
-                    tf_message.clear();
-                }
+                setMessageEvent();
             }
         });
     }
@@ -104,5 +94,30 @@ public class Controller implements Initializable {
                 vbox.getChildren().add(hbox);
             }
         });
+    }
+
+    private void setMessageEvent() {
+        String messageToSend = tf_message.getText();
+        if (!messageToSend.isEmpty()) {
+            HBox hbox = new HBox();
+            hbox.setAlignment(Pos.CENTER_RIGHT);
+            hbox.setPadding(new Insets(5,5,5,10));
+
+            Text text = new Text(messageToSend);
+            TextFlow textFlow = new TextFlow(text);
+
+            textFlow.setStyle("-fx-color: rgb(239,242,255); " +
+                    "-fx-background-color: rgb(15,125,242);" +
+                    " -fx-background-radius: 20px;");
+
+            textFlow.setPadding(new Insets(5,10,5,10));
+            text.setFill(Color.color(0.934,0.945,0.996));
+
+            hbox.getChildren().add(textFlow);
+            vbox_messages.getChildren().add(hbox);
+
+            server.sendMessageToClient(messageToSend);
+            tf_message.clear();
+        }
     }
 }
